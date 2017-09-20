8d5f96b2c890ebcc430ab383bda7749ac41d353e
Zahar <zahar.zolotarev@gmail.com>
2017-09-20 15:26:49 +0300

    fix 2

---
+++ library/src/main/java/com/seu/magicfilter/utils/MagicFilterFactory.java
@@ -0,0 +1,11 @@
+package com.seu.magicfilter.utils;
+
+
+import com.seu.magicfilter.base.gpuimage.GPUImageFilter;
+
+public class MagicFilterFactory {
+
+	public static GPUImageFilter initFilters(MagicFilterType type) {
+		return null;
+	}
+}
